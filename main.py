7246f3d7d057c3769903a64c34819cc850f38756
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-04-19 20:11:48 -0400

    name updates

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import pandas as pd
 
 def main():
     df = pd.read_csv(r"C:\Users\teamj\Documents\regretion_framework\test2.csv")
-    x = ['AGE', 'QUET']
-    x2 = ['AGE', 'QUET', 'SMK']
-    lm = Model(x2, 'SBP', df, True, True)
+    x1x2 = ['AGE', 'QUET']
+    x1x2x3 = ['AGE', 'QUET', 'SMK']
+    x1x3x3 = ['AGE', 'SMK', 'QUET']
+    y = 'SBP'
+    lm = Model(x1x2x3, y, df, True, True)
     print(lm)
     # print(lm.columns)
 
