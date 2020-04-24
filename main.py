7bb2c6e9323b1569a0092446a8f0d797136b8e48
Jayce Slesar <47452474+jayceslesar@users.noreply.github.com>
2020-04-24 16:27:19 -0400

    testing

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pandas as pd
 
 def main():
     df = pd.read_csv(r"C:\Users\teamj\Documents\regretion_framework\test2.csv")
+    x1 = ['AGE']
     x1x2 = ['AGE', 'QUET']
     x1x2x3 = ['AGE', 'QUET', 'SMK']
     x1x3x3 = ['AGE', 'SMK', 'QUET']
     y = 'SBP'
     lm = Model(x1x2x3, y, df, True, True)
+    lm = lm.autobest()
     print(lm)
 
 
